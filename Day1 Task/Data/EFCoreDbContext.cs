c4c09ab80044b09ff56e5c29aa477c6643e9d05d
Student ApnaCollege <ishika.rict21@sot.pdpu.ac.in>
2025-03-06 14:16:06 +0530

    Adding the standar EFCore implementation

---
+++ Day1 Task/Data/EFCoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Day1_Task.Models;
+
+namespace Day1_Task.Data
+{
+    public class EFCoreDbContext: DbContext
+    {
+        public EFCoreDbContext(DbContextOptions<EFCoreDbContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+
+        //public DbSet<Branch> Branches { get; set; }
+
+        //public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Day1 Task/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Day1_Task.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
